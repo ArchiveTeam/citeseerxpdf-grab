57a05e1a1502c1a6656dcb996af2a692f3e630b0
Arkiver2 <Arkiver@hotmail.com>
2016-04-09 00:44:53 +0200

    Fix missing 'urls' list. Bump version to 20160409.02.

--- pipeline.py
+++ pipeline.py
@@ -56,7 +56,7 @@ if not WGET_LUA:
 #
 # Update this each time you make a non-cosmetic change.
 # It will be added to the WARC files and reported to the tracker.
-VERSION = "20160409.01"
+VERSION = "20160409.02"
 USER_AGENT = 'ArchiveTeam'
 TRACKER_ID = 'citeseerxpdf'
 TRACKER_HOST = 'tracker.archiveteam.org'
@@ -195,6 +195,7 @@ class WgetArgs(object):
         assert item_type in ('100urls')
         
         if item_type == '100urls':
+            urls = []
             urllist = requests.get('http://storage.harrycross.me/dev/items/pdf_' + item_value)
             if urllist.status_code == 200:
                 for line in urllist.text.splitlines():
